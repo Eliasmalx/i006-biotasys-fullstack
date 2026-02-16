ac0f22645d4f3a13f17ba3ad46fd32a566e04420
octa-quintero <octa.quinteroo@gmail.com>
2026-02-16 16:51:16 +0100

    NEW // logger morgan funcionando - archivo main.ts configuracion inicial - modulo winston importado en app.module

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,47 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { morganConfig } from './config/morgan.config';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const logger = app.get(WINSTON_MODULE_NEST_PROVIDER);
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  app.useLogger(logger);
+
+  // Configurar Morgan
+  app.use(morganConfig);
+
+  // Configurar CORS
+  app.enableCors({
+    origin: ['http://localhost:3001', 'http://localhost:3000'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'Cache-Control',
+      'Pragma',
+    ],
+  });
+
+  // Configurar ValidationPipe global
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port, () => {
+    console.log(`🚀 Servidor funcionando en el puerto: ${port}`);
+  });
 }
-bootstrap();
+
+void bootstrap();
