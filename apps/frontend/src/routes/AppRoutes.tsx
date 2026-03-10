87bd196116333ba0f2399dc6f66c7b870bab241e
Julio Cesar <juliowork.1309@gmail.com>
2026-03-10 04:17:46 +0000

    cambio en rutas del dashboard

--- apps/frontend/src/routes/AppRoutes.tsx
+++ apps/frontend/src/routes/AppRoutes.tsx
@@ -4,18 +4,19 @@ import ProtectedRoute from "./ProtectedRoute";
 import PublicRoute from "./PublicRoute";
 import Login from "../pages/login/Login";
 import Register from "../pages/Register";
-import Dashboard from "../pages/dashboard/Dashboard";
+import { DashboardNutritionist } from "../pages/dashboard/DashboardNutritionist";
+import { DashboardLaboratory } from "../pages/dashboard/DashboardLaboratory";
 import { AcceptInvitationPage } from "../pages/AcceptInvitation";
 import { PasswordRecovery } from "../pages/login/PasswordRecovery";
 import { PasswordReset } from "../pages/login/PasswordReset";
 import { ForgotPassword } from "../pages/login/ForgotPassword";
-import { DashboardLaboratory } from "../pages/dashboard/DashboardLaboratory";
-import { DashboardNutritionist } from "../pages/dashboard/DashboardNutritionist";
 import { Navbar } from "../pages/Navbar";
 
 export const AppRoutes: React.FC = () => {
   return (
     <Routes>
+
+      {/* Rutas públicas */}
       <Route
         path="/login"
         element={
@@ -24,6 +25,7 @@ export const AppRoutes: React.FC = () => {
           </PublicRoute>
         }
       />
+
       <Route
         path="/register"
         element={
@@ -32,23 +34,38 @@ export const AppRoutes: React.FC = () => {
           </PublicRoute>
         }
       />
+
+      {/* Dashboard Nutricionista */}
       <Route
-        path="/dashboard"
+        path="/dashboardNutritionist"
         element={
-          <ProtectedRoute>
-            <Navbar/>
-            <Dashboard />
+          <ProtectedRoute role="NUTRICIONISTA">
+            <Navbar />
+            <DashboardNutritionist />
           </ProtectedRoute>
         }
       />
+
+      {/* Dashboard Laboratorio */}
+      <Route
+        path="/dashboardLaboratory"
+        element={
+          <ProtectedRoute role="LABORATORIO">
+            <Navbar />
+            <DashboardLaboratory />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* Otras rutas */}
       <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/invitations/accept/:token" element={<AcceptInvitationPage />} />
-      <Route path="/passwoedRecovery" element={<PasswordRecovery />} />
-      <Route path="/passwoedReset" element={<PasswordReset />} />
-      <Route path="/dashboardLaboratory" element={<DashboardNutritionist/>} />
-      
+      <Route path="/passwordRecovery" element={<PasswordRecovery />} />
+      <Route path="/passwordReset" element={<PasswordReset />} />
+
     </Routes>
   );
 };
 
 export default AppRoutes;
+
